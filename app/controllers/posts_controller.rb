1514b4c2bc0bcc1dab1cdc5dc50da9a9b7881386
maroro0220 <maro1339@gamil.com>
2018-01-02 02:40:04 +0000

    Create

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def index
+    @posts=Post.all.reverse
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(title: params[:title], username: params[:username], content: params[:content])
+    redirect_to '/'
+  end
+
+  def read
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
